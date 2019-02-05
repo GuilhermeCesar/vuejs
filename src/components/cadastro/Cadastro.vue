e2f794951af70101a5cc133faba9faece07ac1f6
Guilherme César Medeiros <guilherme.medeiros@terceiros.calcard.com.br>
2019-02-05 08:57:34 -0200

    Prevent default no form

--- src/components/cadastro/Cadastro.vue
+++ src/components/cadastro/Cadastro.vue
@@ -2,23 +2,23 @@
 
   <div>
     <h1 class="centralizado">Cadastro</h1>
-    <h2 class="centralizado"></h2>
+    <h2 class="centralizado">{{ foto.titulo }}</h2>
 
-    <form>
+    <form @submit.prevent="grava" >
       <div class="controle">
         <label for="titulo">TÍTULO</label>
-        <input id="titulo" autocomplete="off">
+        <input id="titulo" autocomplete="off" @input="foto.titulo = $event.target.value" :value="foto.titulo">
       </div>
 
       <div class="controle">
         <label for="url">URL</label>
-        <input id="url" autocomplete="off">
+        <input id="url" autocomplete="off"  @input="foto.url = $event.target.value" :value="foto.url">
         <imagem-responsiva/>
       </div>
 
       <div class="controle">
         <label for="descricao">DESCRIÇÃO</label>
-        <textarea id="descricao" autocomplete="off"></textarea>
+        <textarea id="descricao" autocomplete="off"  @input="foto.descricao = $event.target.value"  :value="foto.descricao"></textarea>
       </div>
 
       <div class="centralizado">
@@ -41,7 +41,28 @@
 
       'imagem-responsiva': ImagemResponsiva,
       'meu-botao': Botao
+    },
+    data(){
+      return {
+        foto:{
+          titulo:'',
+          url:'',
+          descricao:''
+        }
+
+      }
+    },
+    methods:{
+      grava(){
+        console.log('enviar dados para api')
+        this.foto = {
+          titulo:'',
+          url:'',
+          descricao:''
+        }
+      }
     }
+
   }
 
 </script>
